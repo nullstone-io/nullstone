b40075bd4ae61e1f43baaf5468d4f779ee2733aa
Brad Sickles <brad.sickles@gmail.com>
2021-05-03 14:55:02 -0400

    code comment

--- app/container/aws-fargate/provider.go
+++ app/container/aws-fargate/provider.go
@@ -42,6 +42,7 @@ func (p Provider) Push(nsConfig api.Config, app *types.Application, workspace *t
 	sourceUrl := docker.ParseImageUrl(userConfig["source"])
 
 	targetUrl := ic.Outputs.ImageRepoUrl
+	// NOTE: We expect --version from the user which is used as the image for the pushed image
 	if imageTag := userConfig["version"]; imageTag != "" {
 		targetUrl.Tag = imageTag
 	} else {
