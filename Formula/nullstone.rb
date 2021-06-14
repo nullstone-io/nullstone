71e6cf6b6b7296b2fc1989682886b1bee153118b
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-14 17:24:32 +0000

    Brew formula update for nullstone version v0.0.23

--- Formula/nullstone.rb
+++ Formula/nullstone.rb
@@ -5,29 +5,29 @@
 class Nullstone < Formula
   desc "Launch apps on your cloud in minutes"
   homepage "https://nullstone.io"
-  version "0.0.22"
+  version "0.0.23"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.22/nullstone_0.0.22_Darwin_x86_64.tar.gz"
-      sha256 "d3d7b85d08bbcb0d8443f7d808248104502337147c2297be211e89f22824bdb7"
+      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.23/nullstone_0.0.23_Darwin_x86_64.tar.gz"
+      sha256 "0bfa5295edf0fc9c8370adfba47b6ab491d7c2cbec1ce8cff826e9b976d806b5"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.22/nullstone_0.0.22_Darwin_arm64.tar.gz"
-      sha256 "bafcc05778439134e4b79e41036f84e9ce76ebd23e69ab221e7d0bd32e2237d8"
+      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.23/nullstone_0.0.23_Darwin_arm64.tar.gz"
+      sha256 "64153159705de25bee7c9c31cc5b829e55a6eea0a8703ad292fd28c1d26d382e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.22/nullstone_0.0.22_Linux_x86_64.tar.gz"
-      sha256 "e5fd7bf8f4b78f0f7f2b6ef33d61e58f1f427f8a085132f9e6313ecf4f9ee2e6"
+      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.23/nullstone_0.0.23_Linux_x86_64.tar.gz"
+      sha256 "536f74f0221a662e51d48906bf2410090b63ecf5275446d58000e6890f6b417f"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.22/nullstone_0.0.22_Linux_arm64.tar.gz"
-      sha256 "51792e5f1d545b6e251c2293362f2389c62a0256d9ef20bb6d272304df2aa7ad"
+      url "https://github.com/nullstone-io/nullstone/releases/download/v0.0.23/nullstone_0.0.23_Linux_arm64.tar.gz"
+      sha256 "87c55b22aaf9ad6ff2c39247fc53b34dcb586794e1603216c29af43a602f8ece"
     end
   end
 
