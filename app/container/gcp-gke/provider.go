471408b28b13b7e46795f71499232cbaeb5cf46f
Brad Sickles <brad.sickles@gmail.com>
2021-12-29 22:00:31 -0500

    First draft of deploy

--- app/container/gcp-gke/provider.go
+++ app/container/gcp-gke/provider.go
@@ -37,8 +37,43 @@ func (p Provider) Push(nsConfig api.Config, details app.Details, userConfig map[
 	return fmt.Errorf("Not supported yet")
 }
 
+// Deploy takes the following steps to deploy a GCP GKE pod
+//   Get pod
+//   Change image tag
+//   Update pod
 func (p Provider) Deploy(nsConfig api.Config, details app.Details, userConfig map[string]string) error {
-	return fmt.Errorf("Not supported yet")
+	ic, err := p.identify(nsConfig, details)
+	if err != nil {
+		return err
+	}
+
+	pod, err := ic.GetPod()
+	if err != nil {
+		return fmt.Errorf("error retrieving pod: %w", err)
+	}
+	spec := pod.Spec
+
+	logger.Printf("Deploying app %q\n", details.App.Name)
+	version := userConfig["version"]
+	if version != "" {
+		logger.Printf("Updating app version to %q\n", version)
+		if err := app.UpdateVersion(nsConfig, details.App.Id, details.Env.Name, version); err != nil {
+			return fmt.Errorf("error updating app version in nullstone: %w", err)
+		}
+
+		logger.Printf("Updating image tag to %q\n", version)
+		if spec, err = ic.ReplacePodSpecImageTag(pod.Spec, version); err != nil {
+			return fmt.Errorf("error updating pod spec with new image tag: %w", err)
+		}
+	}
+
+	pod.Spec = spec
+	if _, err := ic.UpdatePod(pod); err != nil {
+		return fmt.Errorf("error deploying service: %w", err)
+	}
+
+	logger.Printf("Deployed app %q\n", details.App.Name)
+	return nil
 }
 
 func (p Provider) Status(nsConfig api.Config, details app.Details) (app.StatusReport, error) {
