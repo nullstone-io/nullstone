b90a9c697a4c34efb81cbcf45607b2acdf0d2b61
Brad Sickles <brad.sickles@gmail.com>
2022-02-17 08:48:39 -0500

    Using built-in function for handling os signals in context

--- cmd/cancellable_action.go
+++ cmd/cancellable_action.go
@@ -9,14 +9,6 @@ import (
 
 func CancellableAction(fn func(ctx context.Context) error) error {
 	ctx := context.Background()
-	// Handle Ctrl+C, kill stream
-	ctx, cancelFn := context.WithCancel(ctx)
-	defer cancelFn()
-	term := make(chan os.Signal, 1)
-	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-term
-		cancelFn()
-	}()
+	signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
 	return fn(ctx)
 }
