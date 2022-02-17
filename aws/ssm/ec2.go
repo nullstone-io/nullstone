0fa3429d26d797ff5c0c1255cca3648246b7c917
Brad Sickles <brad.sickles@gmail.com>
2022-02-17 10:24:25 -0500

    Added notes about ec2 ssh requirements

--- aws/ssm/ec2.go
+++ aws/ssm/ec2.go
@@ -8,6 +8,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
+// StartEc2Session initiates an interactive SSH session with an EC2 instance using SSM
+// See setup guide: https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-getting-started.html
+// In short, the following is necessary for this function to work
+//   - ec2 instance has SSM agent installed and registered
+//   - ec2 instance is configured with instance profile that has the AmazonSSMManagedInstanceCore policy attached (or an equivalent custom policy)
+//   - config contains an AWS identity that has access to ssm:StartSession on the EC2 Instance
 func StartEc2Session(config aws.Config, region, instanceId string) error {
 	ctx := context.Background()
 
