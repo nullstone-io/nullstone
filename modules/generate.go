745e171ac37681e24eda9d8b00bd12f5a2e6a432
Brad Sickles <brad.sickles@gmail.com>
2022-02-21 18:00:06 -0500

    Added subdomain_dns_name to nullstone.tf for subdomains

--- modules/generate.go
+++ modules/generate.go
@@ -50,6 +50,10 @@ data "ns_subdomain" "this" {
   stack_id = data.ns_workspace.this.stack_id
   block_id = data.ns_workspace.this.block_id
 }
+
+locals {
+  subdomain_dns_name = data.ns_subdomain.this.dns_name
+}
 `
 )
 
