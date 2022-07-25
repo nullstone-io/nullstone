a8dbdf76d4876a618c8a6a4fb988e17b21705c08
Brad Sickles <brad.sickles@gmail.com>
2022-07-25 18:37:26 -0400

    Add back code to CreateDeploy record in database

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -7,6 +7,7 @@ import (
 	"github.com/nullstone-io/deployment-sdk/logging"
 	"github.com/urfave/cli/v2"
 	"gopkg.in/nullstone-io/go-api-client.v0"
+	"log"
 )
 
 var Deploy = func(providers app.Providers) *cli.Command {
@@ -39,4 +40,22 @@ var Deploy = func(providers app.Providers) *cli.Command {
 			})
 		},
 	}
+}
+
+// TODO: Migrate CLI to use CreateDeploy instead of performing locally?
+func CreateDeploy(nsConfig api.Config, stackId, appId, envId int64, version string) error {
+	if version == "" {
+		return fmt.Errorf("no version specified, version is required to create a deploy")
+	}
+
+	client := api.Client{Config: nsConfig}
+	result, err := client.Deploys().Create(stackId, appId, envId, version)
+	if err != nil {
+		return fmt.Errorf("error updating app version: %w", err)
+	} else if result == nil {
+		return fmt.Errorf("could not find application environment")
+	}
+
+	log.Println("Deployment created")
+	return nil
 }
