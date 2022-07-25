81804a9f96a1e70fe7035e835c8021997f717620
Brad Sickles <brad.sickles@gmail.com>
2022-07-25 16:34:50 -0400

    Use deployment-sdk for deploy command

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -2,9 +2,12 @@ package cmd
 
 import (
 	"context"
+	"fmt"
+	"github.com/nullstone-io/deployment-sdk/app"
+	"github.com/nullstone-io/deployment-sdk/logging"
 	"github.com/urfave/cli/v2"
 	"gopkg.in/nullstone-io/go-api-client.v0"
-	"gopkg.in/nullstone-io/nullstone.v0/app"
+	"time"
 )
 
 var Deploy = func(providers app.Providers) *cli.Command {
@@ -19,11 +22,48 @@ var Deploy = func(providers app.Providers) *cli.Command {
 			AppVersionFlag,
 		},
 		Action: func(c *cli.Context) error {
-			return AppEnvAction(c, providers, func(ctx context.Context, cfg api.Config, provider app.Provider, details app.Details) error {
-				userConfig := map[string]string{
-					"version": DetectAppVersion(c),
+			return AppWorkspaceAction(c, func(ctx context.Context, cfg api.Config, appDetails app.Details) error {
+				deployer, err := providers.FindDeployer(logging.StandardOsWriters{}, cfg, appDetails)
+				if err != nil {
+					return fmt.Errorf("error creating app deployer: %w", err)
+				} else if deployer == nil {
+					return fmt.Errorf("this CLI does not support application category=%s, type=%s", appDetails.Module.Category, appDetails.Module.Type)
 				}
-				return provider.Deploy(cfg, details, userConfig)
+				reference, err := deployer.Deploy(ctx, DetectAppVersion(c))
+				if err != nil {
+					return err
+				}
+
+				deployStatusGetter, err := providers.FindDeployStatusGetter(logging.StandardOsWriters{}, cfg, appDetails)
+				if err != nil {
+					return fmt.Errorf("error creating app deployment status analyzer: %w", err)
+				} else if deployStatusGetter == nil {
+					// If we don't have a way of retrieving status, we are just going to complete the deployment
+					return nil
+				}
+
+				for {
+					status, err := deployStatusGetter.GetDeployStatus(ctx, reference)
+					if err != nil {
+						return fmt.Errorf("error querying app deployment status: %w", err)
+					}
+					switch status {
+					case app.RolloutStatusComplete:
+						return nil
+					case app.RolloutStatusInProgress:
+						return nil
+					case app.RolloutStatusFailed:
+						return fmt.Errorf("deployment failed")
+					case app.RolloutStatusUnknown:
+						return fmt.Errorf("unknown app deployment status")
+					}
+
+					select {
+					case <-ctx.Done():
+						return fmt.Errorf("cancelled")
+					case <-time.After(5 * time.Second):
+					}
+				}
 			})
 		},
 	}
