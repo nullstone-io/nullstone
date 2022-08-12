d1c9ba200a277ad52da3b28c1428e70f76098483
Brad Sickles <brad.sickles@gmail.com>
2022-08-11 20:43:31 -0400

    C955 deploy live logs (#94)

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/nullstone-io/deployment-sdk/app"
-	"github.com/nullstone-io/deployment-sdk/logging"
 	"github.com/urfave/cli/v2"
 	"gopkg.in/nullstone-io/go-api-client.v0"
+	"gopkg.in/nullstone-io/go-api-client.v0/types"
+	"gopkg.in/nullstone-io/go-api-client.v0/ws"
+	"os"
+	"time"
 )
 
 var Deploy = func(providers app.Providers) *cli.Command {
@@ -30,50 +33,45 @@ var Deploy = func(providers app.Providers) *cli.Command {
 				if version == "" {
 					return fmt.Errorf("no version specified, version is required to create a deploy")
 				}
-				osWriters := logging.StandardOsWriters{}
-				provider := providers.FindFactory(*appDetails.Module)
-				if provider == nil {
-					return fmt.Errorf("deploy is not supported for this app")
-				}
-				if err := CreateDeploy(cfg, appDetails, version); err != nil {
-					return err
-				}
-				reference, err := deploy(ctx, cfg, appDetails, osWriters, provider, version)
+
+				deploy, err := CreateDeploy(cfg, appDetails, version)
 				if err != nil {
 					return err
 				}
-				if wait {
-					if err := waitHealthy(ctx, cfg, appDetails, osWriters, provider, reference); err != nil {
-						return err
-					}
-				}
-				return nil
+				return streamDeployLogs(ctx, cfg, *deploy, wait)
 			})
 		},
 	}
 }
 
-func deploy(ctx context.Context, cfg api.Config, appDetails app.Details, osWriters logging.OsWriters, provider *app.Provider, version string) (string, error) {
-	if provider.NewDeployer == nil {
-		return "", fmt.Errorf("This app does not support deploy.")
+func streamDeployLogs(ctx context.Context, cfg api.Config, deploy types.Deploy, wait bool) error {
+	fmt.Fprintln(os.Stderr, "Waiting for logs...")
+	client := api.Client{Config: cfg}
+	msgs, err := client.DeployLiveLogs().Watch(ctx, deploy.StackId, deploy.Id, ws.RetryInfinite(time.Second))
+	if err != nil {
+		return fmt.Errorf("error connecting to deploy logs: %w", err)
+	}
+	for msg := range msgs {
+		if msg.Source == "error" {
+			return fmt.Errorf(msg.Content)
+		}
+		if !wait && msg.Context == types.DeployPhaseWaitHealthy {
+			// Stop streaming logs if we receive a log message from wait-healthy and no --wait
+			break
+		}
+		fmt.Fprint(os.Stderr, msg.Content)
 	}
 
-	stdout := osWriters.Stdout()
-	fmt.Fprintln(stdout, "Deploying app...")
-	deployer, err := provider.NewDeployer(osWriters, cfg, appDetails)
+	updated, err := client.Deploys().Get(deploy.StackId, deploy.AppId, deploy.EnvId, deploy.Id)
 	if err != nil {
-		return "", fmt.Errorf("error creating app deployer: %w", err)
-	} else if deployer == nil {
-		return "", fmt.Errorf("deploy is not supported for this app")
+		return fmt.Errorf("error retrieving deploy status: %w", err)
 	}
-	reference, err := deployer.Deploy(ctx, version)
-	if err != nil {
-		return "", fmt.Errorf("error deploying app: %w", err)
-	} else if reference == "" {
-		return "", nil
+	fmt.Fprintln(os.Stdout, updated.Reference)
+	switch updated.Status {
+	case types.DeployStatusCancelled:
+		return fmt.Errorf("Deploy was cancelled.")
+	case types.DeployStatusFailed:
+		return fmt.Errorf("Deploy failed to complete: %s", updated.StatusMessage)
 	}
-	fmt.Fprintln(stdout, "App deployed.")
-	fmt.Fprintf(osWriters.Stdout(), "Deploy ID: %s\n", reference)
-	fmt.Fprintln(stdout, "")
-	return reference, nil
+	return nil
 }
--- go.mod
+++ go.mod
@@ -10,13 +10,13 @@ require (
 	github.com/fatih/color v1.13.0
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/gosuri/uilive v0.0.4
-	github.com/nullstone-io/deployment-sdk v0.0.0-20220728195859-d60a87d3aa61
+	github.com/nullstone-io/deployment-sdk v0.0.0-20220809192114-0f6ea2b23e4d
 	github.com/nullstone-io/module v0.2.8
 	github.com/ryanuber/columnize v2.1.2+incompatible
 	github.com/stretchr/testify v1.7.0
 	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
-	gopkg.in/nullstone-io/go-api-client.v0 v0.0.0-20220802213317-caaf549369ff
+	gopkg.in/nullstone-io/go-api-client.v0 v0.0.0-20220811142623-bf5603c6482f
 )
 
 require (
