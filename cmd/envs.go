17e0a7c9c27b8760cfd611ba0f97f8f34dc45947
Scott Sickles <scottsickles@gmail.com>
2022-10-26 15:51:18 -0400

    added the `envs up` and `envs down` commands

--- cmd/envs.go
+++ cmd/envs.go
@@ -24,6 +24,8 @@ var Envs = &cli.Command{
 	Subcommands: []*cli.Command{
 		EnvsList,
 		EnvsNew,
+		EnvsUp,
+		EnvsDown,
 	},
 }
 
@@ -136,15 +138,15 @@ var EnvsNew = &cli.Command{
 			}
 
 			if preview {
-				return CreatePreviewEnv(client, stack.Id, name)
+				return createPreviewEnv(client, stack.Id, name)
 			} else {
-				return CreatePipelineEnv(client, stack.Id, name, providerName, region, zone)
+				return createPipelineEnv(client, stack.Id, name, providerName, region, zone)
 			}
 		})
 	},
 }
 
-func CreatePipelineEnv(client api.Client, stackId int64, name, providerName, region, zone string) error {
+func createPipelineEnv(client api.Client, stackId int64, name, providerName, region, zone string) error {
 	provider, err := client.Providers().Get(providerName)
 	if err != nil {
 		return fmt.Errorf("error looking for provider %q: %w", providerName, err)
@@ -187,11 +189,79 @@ func CreatePipelineEnv(client api.Client, stackId int64, name, providerName, reg
 	return nil
 }
 
-func CreatePreviewEnv(client api.Client, stackId int64, name string) error {
+func createPreviewEnv(client api.Client, stackId int64, name string) error {
 	env, err := client.PreviewEnvs().Create(stackId, &api.CreatePreviewEnvInput{Name: name})
 	if err != nil {
 		return fmt.Errorf("error creating preview environment: %w", err)
 	}
 	fmt.Printf("created %q preview environment\n", env.Name)
+	return nil
+}
+
+var EnvsUp = &cli.Command{
+	Name:        "up",
+	Description: "Launches an entire environment including all of its apps. This command can be used to stand up an entire preview environment.",
+	Usage:       "Launch an entire environment",
+	UsageText:   "nullstone envs up --stack=<stack> --env=<env>",
+	Flags: []cli.Flag{
+		StackFlag,
+		EnvFlag,
+	},
+	Action: func(c *cli.Context) error {
+		return ProfileAction(c, func(cfg api.Config) error {
+			return createEnvRun(c, cfg, false)
+		})
+	},
+}
+
+var EnvsDown = &cli.Command{
+	Name:        "down",
+	Description: "Destroys all the apps in an environment and all their dependent infrastructure. This command is useful for tearing down preview environments once you are finished with them.",
+	Usage:       "Destroy an entire environment",
+	UsageText:   "nullstone envs down --stack=<stack> --env=<env>",
+	Flags: []cli.Flag{
+		StackFlag,
+		EnvFlag,
+	},
+	Action: func(c *cli.Context) error {
+		return ProfileAction(c, func(cfg api.Config) error {
+			return createEnvRun(c, cfg, true)
+		})
+	},
+}
+
+func createEnvRun(c *cli.Context, cfg api.Config, isDestroy bool) error {
+	client := api.Client{Config: cfg}
+	stackName := c.String("stack")
+	envName := c.String("env")
+
+	stack, err := client.StacksByName().Get(stackName)
+	if err != nil {
+		return fmt.Errorf("error looking for stack %q: %w", stackName, err)
+	} else if stack == nil {
+		return fmt.Errorf("stack %q does not exist", stackName)
+	}
+
+	env, err := find.Env(cfg, stack.Id, envName)
+	if err != nil {
+		return fmt.Errorf("error looking for environment in stack %d - %q: %w", stack.Id, envName, err)
+	} else if env == nil {
+		return fmt.Errorf("environment %q does not exist in stack %d", envName, stack.Id)
+	}
+
+	body := types.CreateEnvRunInput{IsDestroy: isDestroy}
+	runs, err := client.EnvRuns().Create(stack.Id, env.Id, body)
+	if err != nil {
+		if isDestroy {
+			return fmt.Errorf("error creating run to destroy environment: %w", err)
+		} else {
+			return fmt.Errorf("error creating run to launch environment: %w", err)
+		}
+	}
+	if isDestroy {
+		fmt.Printf("created run to destroy %d apps in the %q environment\n", len(runs), envName)
+	} else {
+		fmt.Printf("created run to launch %d apps in the %q environment\n", len(runs), envName)
+	}
 	return nil
 }
