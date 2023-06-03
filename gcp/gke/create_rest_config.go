472db7c1f7be392c528766eb6fda00170c47cc89
Brad Sickles <brad.sickles@gmail.com>
2023-06-03 11:19:15 -0400

    Fix initialization of rest client to k8s cluster for ssh

--- gcp/gke/create_rest_config.go
+++ gcp/gke/create_rest_config.go
@@ -5,6 +5,8 @@ import (
 	"github.com/nullstone-io/deployment-sdk/gcp"
 	"github.com/nullstone-io/deployment-sdk/gcp/gke"
 	"github.com/nullstone-io/deployment-sdk/k8s"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 )
 
@@ -13,5 +15,11 @@ func CreateRESTConfig(ctx context.Context, serviceAccount gcp.ServiceAccount, cl
 		TokenSourcer:  serviceAccount,
 		ClusterInfoer: cluster,
 	}
-	return configCreator.Create(ctx, gke.GcpScopes...)
+	cfg, err := configCreator.Create(ctx, gke.GcpScopes...)
+	if err != nil {
+		return nil, err
+	}
+	cfg.GroupVersion = &schema.GroupVersion{Group: "", Version: "v1"}
+	cfg.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
+	return cfg, nil
 }
