4c164fb5984ab423042012c433900fc82abe314f
Brad Sickles <brad.sickles@gmail.com>
2024-02-20 09:28:16 -0500

    Limit tracking run to "launch"

--- cmd/wait_for_launch.go
+++ cmd/wait_for_launch.go
@@ -33,22 +33,19 @@ func WaitForLaunch(ctx context.Context, osWriters logging.OsWriters, cfg api.Con
 
 	// If we made it here, we want to wait for a launch and the workspace has not been provisioned yet
 	// Let's look for all the runs on this workspace to see if there are any pending
-	// If all runs we retrieve are terminal, we use the latest to determine whether we can proceed
-	ntRuns, err := find.NonTerminalRuns(cfg, appDetails.App.StackId, ws.Uid)
+	// If we retrieve non-terminal runs, we use the oldest (next in line) to determine whether we can proceed
+	launchRun, err := findLaunchRun(cfg, *appDetails)
 	if err != nil {
 		return err
-	}
-	if len(ntRuns) <= 0 {
+	} else if launchRun == nil {
 		return fmt.Errorf("app %q has not been provisioned in %q environment yet", appDetails.App.Name, appDetails.Env.Name)
 	}
 
-	track := ntRuns[0]
-
 	stderr := osWriters.Stderr()
 	fmt.Fprintf(stderr, "Waiting for app %q to launch in %q environment...\n", appDetails.App.Name, appDetails.Env.Name)
-	fmt.Fprintf(stderr, "Watching run for launch: %s\n", runs.GetBrowserUrl(cfg, ws, track))
+	fmt.Fprintf(stderr, "Watching run for launch: %s\n", runs.GetBrowserUrl(cfg, ws, *launchRun))
 
-	result, err := runs.WaitForTerminal(ctx, osWriters, cfg, ws, track)
+	result, err := runs.WaitForTerminal(ctx, osWriters, cfg, ws, *launchRun)
 	if err != nil {
 		return err
 	} else if result.Status == types.RunStatusCompleted {
@@ -65,4 +62,21 @@ func WaitForLaunch(ctx context.Context, osWriters logging.OsWriters, cfg api.Con
 	}
 	fmt.Fprintf(stderr, "App failed to launch because run finished with %q status.\n", result.Status)
 	return fmt.Errorf("Could not run command because app failed to launch")
+}
+
+func findLaunchRun(cfg api.Config, appDetails app.Details) (*types.Run, error) {
+	ntRuns, err := find.NonTerminalRuns(cfg, appDetails.App.StackId, appDetails.Workspace.Uid)
+	if err != nil {
+		return nil, err
+	}
+
+	// It's possible that the queue of runs contains a "destroy" run
+	// Let's find the first that is a launch/apply
+	for _, run := range ntRuns {
+		if run.IsDestroy {
+			continue
+		}
+		return &run, nil
+	}
+	return nil, nil
 }
