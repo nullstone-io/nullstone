68394b959c30a593110db06cb1e1b37c01d0bb2d
Brad Sickles <brad.sickles@gmail.com>
2026-02-04 12:46:32 -0500

    Implement EKS remoter

---
+++ aws/eks/exec_command.go
@@ -0,0 +1,31 @@
+package eks
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/nullstone-io/deployment-sdk/aws/eks"
+	"gopkg.in/nullstone-io/nullstone.v0/k8s"
+)
+
+func ExecCommand(ctx context.Context, infra Outputs, pod, container string, cmd []string, opts *k8s.ExecOptions) error {
+	cfg, err := eks.CreateKubeConfig(ctx, infra.ClusterNamespace, infra.Deployer)
+	if err != nil {
+		return fmt.Errorf("error creating kube config: %w", err)
+	}
+
+	podName, err := k8s.GetPodName(ctx, cfg, infra.ServiceNamespace, infra.ServiceName, pod)
+	if err != nil {
+		return fmt.Errorf("error finding pod: %w", err)
+	}
+
+	if container == "" {
+		container = infra.MainContainerName
+	}
+
+	if len(cmd) == 0 {
+		cmd = []string{"/bin/sh"}
+	}
+
+	return k8s.ExecCommand(ctx, cfg, infra.ServiceNamespace, podName, container, cmd, opts)
+}
