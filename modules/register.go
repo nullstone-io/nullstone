e90c7cd7e76485166c9bc8e3f35f6d6cf0ff87b4
Brad Sickles <brad.sickles@gmail.com>
2026-02-04 16:29:35 -0500

    Added SourceUrl to module registration

--- modules/register.go
+++ modules/register.go
@@ -2,30 +2,28 @@ package modules
 
 import (
 	"context"
+
 	"gopkg.in/nullstone-io/go-api-client.v0"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 )
 
 func Register(ctx context.Context, cfg api.Config, manifest *types.ModuleManifest) (*types.Module, error) {
-	module := &types.Module{
-		OrgName:       manifest.OrgName,
+	input := api.CreateModuleInput{
 		Name:          manifest.Name,
 		FriendlyName:  manifest.FriendlyName,
 		Description:   manifest.Description,
 		IsPublic:      manifest.IsPublic,
-		Category:      types.CategoryName(manifest.Category),
-		Subcategory:   types.SubcategoryName(manifest.Subcategory),
+		Category:      manifest.Category,
+		Subcategory:   manifest.Subcategory,
 		ProviderTypes: manifest.ProviderTypes,
 		Platform:      manifest.Platform,
 		Subplatform:   manifest.Subplatform,
 		AppCategories: manifest.AppCategories,
 		Type:          manifest.Type,
-		Status:        types.ModuleStatusPublished,
+		Status:        string(types.ModuleStatusPublished),
+		SourceUrl:     manifest.SourceUrl,
 	}
 
 	client := api.Client{Config: cfg}
-	if err := client.Modules().Create(ctx, module.OrgName, module); err != nil {
-		return nil, err
-	}
-	return client.Modules().Get(ctx, module.OrgName, module.Name)
+	return client.Modules().Create(ctx, manifest.OrgName, input)
 }
